fd511fd1cd3142e3c66bd126ea4af4204af73e10
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-08-27 20:47:59 -0700

    add new pizzaOrder obbject so customer can make multiple orders in future

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
-function pizzaOrder(first, last, address, phone, size, toppings) {
+function pizzaCustomer(first, last, address, phone, orders) {
   this.first = first;
   this.last = last;
   this.address = address;
-  this.phone = phone
+  this.phone = phone;
+  this.orders = orders;
+  this.totalPrice = 0;
+}
+
+function pizzaOrder(size, toppings) {
   this.sizing = size;
   this.toppings = toppings;
   this.price = 0;
@@ -24,7 +29,17 @@ pizzaOrder.prototype.pricing = function() {
   this.price += 0.50 * this.sizing.length;
 }
 
+pizzaCustomer.prototype.totalPricing = function() {
+  for (var i = 0; i < this.orders.length; i++) {
+    this.orders[i].pricing();
+    this.totalPrice += this.orders[i].price;
+  }
+}
+
 $(function() {
+  var ordArray = [];
+  var inputSize = "";
+  var inputToppings = [];
   $("#orderButton").click(function() {
     $("#welcomeScreen").slideUp();
     $("#chooseSize").show();
@@ -32,10 +47,17 @@ $(function() {
   $("#sizeButton").click(function() {
     $("#sizeButton").slideUp();
     $("#chooseToppings").fadeIn(1500);
+    inputSize = $("input:radio[name=size]:checked").val();
   });
   $("#toppingsButton").click(function() {
     $("#toppingsButton").slideUp();
     $("#userInfo").fadeIn(1500);
+    $("input:checkbox[name=topping]:checked").each(function() {
+      inputToppings.push(" " + this.value);
+    })
+    var order = new pizzaOrder(inputSize, inputToppings);
+    ordArray.push(order);
+    console.log(order);
   });
   $("form").submit(function(event) {
     event.preventDefault();
@@ -43,19 +65,14 @@ $(function() {
         inputLast = $("#lastname-input").val();
         inputAddress = $("#address-input").val();
         inputPhone = $("#phone-input").val();
-        inputSize = $("input:radio[name=size]:checked").val();
-        inputToppings = [];
-    $("input:checkbox[name=topping]:checked").each(function() {
-      inputToppings.push(" " + this.value);
-    })
-    var order = new pizzaOrder(inputFirst, inputLast, inputAddress, inputPhone, inputSize, inputToppings);
-    order.pricing();
-    $("#name").text(order.first + " " + order.last);
-    $("#address").text(order.address);
-    $("#phone").text(order.phone);
-    $("#size").text(order.sizing);
-    $("#toppings").text(order.toppings);
-    $("#price").text("$" + order.price.toFixed(2));
+    var customer = new pizzaCustomer(inputFirst, inputLast, inputAddress, inputPhone, ordArray);
+    customer.totalPricing();
+    $("#name").text(customer.first + " " + customer.last);
+    $("#address").text(customer.address);
+    $("#phone").text(customer.phone);
+    $("#size").text(customer.orders[0].sizing);
+    $("#toppings").text(customer.orders[0].toppings);
+    $("#price").text("$" + customer.totalPrice.toFixed(2));
     $("form").slideUp();
     $("#finishedOrder").fadeIn(1500);
     console.log(order);
